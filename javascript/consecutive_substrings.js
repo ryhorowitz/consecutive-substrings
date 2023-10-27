abc44be773f81492560918753fe0f25fe15c3370
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-10-27 16:03:29 -0400

    toy problem complete both js and ruby

--- javascript/consecutive_substrings.js
+++ javascript/consecutive_substrings.js
@@ -1,6 +1,23 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const results = []
+  strArr = string.split('')
+
+  strArr.forEach((char, i) => {
+    results.push(char)
+    let substring = char
+    let j = i
+
+    while (string[j + 1] &&
+      string[j].charCodeAt() === string[j + 1].charCodeAt() - 1) {
+      substring += string[j + 1]
+      results.push(substring)
+      j++
+    }
+  })
+
+  return results
 }
+
 
 if (require.main === module) {
   // add your own tests in here
